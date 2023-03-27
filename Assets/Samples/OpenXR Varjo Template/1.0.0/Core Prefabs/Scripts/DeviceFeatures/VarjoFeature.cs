b2d34fa636ebb7850a7082b76f6df212b47b362a
fp-hive <48753667+fp-hive@users.noreply.github.com>
2023-03-27 11:19:42 +0200

    update exposure

--- Assets/Samples/OpenXR Varjo Template/1.0.0/Core Prefabs/Scripts/DeviceFeatures/VarjoFeature.cs
+++ Assets/Samples/OpenXR Varjo Template/1.0.0/Core Prefabs/Scripts/DeviceFeatures/VarjoFeature.cs
@@ -259,6 +259,7 @@ namespace DeviceFeatures
                     _metadataFrame = _cameraSubsystem.MetadataStream.GetFrame();
                     float exposureValue = (float)_metadataFrame.metadata.ev +
                                           Mathf.Log((float)_metadataFrame.metadata.cameraCalibrationConstant, 2f);
+                    Debug.Log((float)_metadataFrame.metadata.ev + " " + (float)_metadataFrame.metadata.cameraCalibrationConstant);
                     _volumeExposure.fixedExposure.Override(exposureValue);
 
                     _volumeSky.hdriSky.Override(_cubemapFrame.cubemap);
